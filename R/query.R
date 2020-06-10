e13423d139319da5b30351144e7ae8644dc70b62
Chisholm <ChisholmE@ENT.dfo-mpo.ca>
2020-06-10 11:29:03 -0300

    query function for azmpdata

--- R/query.R
+++ R/query.R
@@ -1,1 +1,91 @@
 # Query azmpdata
+
+# query azmp data
+
+query <- function(search_key, location = 'title'){
+  #' @param search_key Keyword(s) to search for in azmpdata, if using multiple keywords, provide vector of character strings
+  #' @param location Where to search for keyword(s), options are 'title', or 'metadata'
+  #' @param logic character string specifying search logic, options are 'AND' to search for matches to ALL search_key terms, or 'OR' to search for matches to any search_key terms
+  #'
+  #'
+  #'  eg. query(search_key = c('mean', 'chlorophyll'), location = 'metadata')
+
+  search_locs <- c('title', 'metadata')
+  if(!location %in% search_locs){
+    stop('Invalid location argument!')
+  }
+  rda_list <- utils::data(package="azmpdata")
+
+# search titles
+  if(location == 'title'){
+    titles <- rda_list$results[,3]
+
+    res <- list()
+    for(i in 1:length(search_key)){
+      res[[i]] <- grep(titles, pattern = search_key[i], value = TRUE, ignore.case = TRUE)
+    }
+
+    vals <- unique(unlist(res)) #  unique titles which match any search terms
+    all_vals <- unlist(res) # all titles which match any search terms
+    names(res) <- search_key
+
+    # count how many times title is repeated
+    rep <- list()
+    for(v in vals){
+      rep[[v]] <- grep(pattern = v, x = all_vals)
+    }
+
+    # if title is matched to every search term, success
+    final <- list()
+    for(i in 1:length(rep)){
+      if(length(rep[[i]]) == length(search_key)){
+        final[[i]] <- names(rep[i])
+      }
+    }
+
+    # print only titles matching ALL search terms
+    return(unlist(final))
+
+  }
+
+  # search metadata (description)
+  # TODO add other metadta search fields
+  if(location == 'metadata'){
+    titles <- rda_list$results[,3]
+    desc <- rda_list$results[,4]
+
+    res <- list()
+    for(i in 1:length(search_key)){
+      res[[i]] <- grep(desc, pattern = search_key[i], value = TRUE, ignore.case = TRUE)
+    }
+
+    vals <- unique(unlist(res)) #  unique desc which match any search terms
+    all_vals <- unlist(res) # all desc which match any search terms
+    names(res) <- search_key
+
+    # count how many times desc is repeated
+    rep <- list()
+    for(v in vals){
+      rep[[v]] <- grep(pattern = v, x = all_vals)
+    }
+
+    # if desc is matched to every search term, success
+    final <- list()
+    for(i in 1:length(rep)){
+      if(length(rep[[i]]) == length(search_key)){
+        final[[i]] <- names(rep[i])
+      }
+    }
+
+    # match desc to title
+    final_tit <- titles[which(desc == final)]
+
+    # print only titles matching ALL search terms
+    return(unlist(final_tit))
+
+  }
+
+
+
+
+}
