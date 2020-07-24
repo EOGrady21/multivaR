fee0eb286b23a68cc3481b92ab235e6ec62657fc
Chisholm <ChisholmE@ENT.dfo-mpo.ca>
2020-07-24 11:20:30 -0300

    updated pca doc and testing

--- R/pca.R
+++ R/pca.R
@@ -1,10 +1,13 @@
 # PCA function from Dan Reed, modified by E. Chisholm June 2020
 
 #' Principal component analysis for given set of variables
-#'
+#' Performs a principal components analysis using the stats package, on all
+#' variables provided in `data` with the exception of `year`. Converts to
+#' data.table and removes duplicate or incomplete rows before processing.
 #'
 #' @param data a datatable or dataframe with varibles to be analyzed for PCA
 #'
+#' @author Daniel Reed, Emily Chisholm
 #' @return
 #' @export
 #'
@@ -20,7 +23,7 @@ PCA <- function(data){
   dt <- na.omit(unique(data))
 
   # Perform PCA for given months, station and variables
-  res <- prcomp(x = na.omit(dt[, -"year"]), scale. = TRUE, center = TRUE)
+  res <- stats::prcomp(x = na.omit(dt[, -"year"]), scale. = TRUE, center = TRUE)
 
   # Add time to the scores for plotting purposes
   res$x <- data.table(year = dt$year, res$x)
---
+++ tests/testthat/test_pca.R
@@ -0,0 +1,23 @@
+# PCA testing
+
+context('Test PCA results')
+
+# create sample data
+data <- data.frame(year = seq(1999, 2015), ice_volume = runif(17), ice_day = runif(17), temperature = runif(17), salinity = runif(17))
+data$ice_volume[c(15, 2, 8)] <- -99
+data <- handle_na(data, na_value = -99, col = 'ice_volume')
+
+test_that('PCA results handle NAs properly' {
+  res <- PCA(data)
+  expect_true(length(res$x$year) == 14)
+})
+
+test_that('PCA handles data.frames and data.tables', {
+
+  dt <- as.data.table(data)
+  resdt <- PCA(dt)
+
+  res <- PCA(data)
+
+  expect_identical(resdt, res)
+})
